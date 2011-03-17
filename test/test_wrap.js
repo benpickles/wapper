ac28e6d9d928acfaac3ca83186f040f59080f4b0
Ben Pickles <spideryoung@gmail.com>
2011-03-17 17:12:30 +0000

    Corresponding wrap tests.

--- test/test_wrap.js
+++ test/test_wrap.js
@@ -31,6 +31,86 @@ test("with containing node", function() {
   assertSameNodes(spans, doc.find("span").get())
 })
 
+test("starting in an inline element", function() {
+  var doc = $("<div>").html('<p>lorem <a href="/">ipsum</a> dolor sit amet.</p>')
+  var p = doc.find("p")[0]
+
+  var spans = Wapper.wrap({
+    commonAncestorContainer: p,
+    startContainer: p.childNodes[1].firstChild,
+    startOffset: 2,
+    endContainer: p.childNodes[2],
+    endOffset: 5
+  })
+
+  equals(doc.html(), '<p>lorem <a href="/">ip<span>sum</span></a><span> dolo</span>r sit amet.</p>')
+  assertSameNodes(spans, doc.find("span").get())
+})
+
+test("starting in an inline element, encompassing nested", function() {
+  var doc = $("<div>").html('<p>lorem <a href="/">ipsum</a> <a href="/">dolor</a> sit amet.</p>')
+  var p = doc.find("p")[0]
+
+  var spans = Wapper.wrap({
+    commonAncestorContainer: p,
+    startContainer: p.childNodes[1].firstChild,
+    startOffset: 2,
+    endContainer: p.childNodes[4],
+    endOffset: 4
+  })
+
+  equals(doc.html(), '<p>lorem <a href="/">ip<span>sum</span></a><span> <a href="/">dolor</a> sit</span> amet.</p>')
+  assertSameNodes(spans, doc.find("span").get())
+})
+
+test("ending in an inline element", function() {
+  var doc = $("<div>").html('<p>lorem ipsum <a href="/">dolor</a> sit amet.</p>')
+  var p = doc.find("p")[0]
+
+  var spans = Wapper.wrap({
+    commonAncestorContainer: p,
+    startContainer: p.firstChild,
+    startOffset: 6,
+    endContainer: p.childNodes[1].firstChild,
+    endOffset: 3
+  })
+
+  equals(doc.html(), '<p>lorem <span>ipsum </span><a href="/"><span>dol</span>or</a> sit amet.</p>')
+  assertSameNodes(spans, doc.find("span").get())
+})
+
+test("ending in an inline element, encompassing nested", function() {
+  var doc = $("<div>").html('<p>lorem ipsum <a href="/">dolor</a> <a href="/">sit</a> amet.</p>')
+  var p = doc.find("p")[0]
+
+  var spans = Wapper.wrap({
+    commonAncestorContainer: p,
+    startContainer: p.firstChild,
+    startOffset: 6,
+    endContainer: p.childNodes[3].firstChild,
+    endOffset: 2
+  })
+
+  equals(doc.html(), '<p>lorem <span>ipsum <a href="/">dolor</a> </span><a href="/"><span>si</span>t</a> amet.</p>')
+  assertSameNodes(spans, doc.find("span").get())
+})
+
+test("starting and ending in an inline element", function() {
+  var doc = $("<div>").html('<p>lorem <a href="/">ipsum</a> <a href="/">dolor</a> sit amet.</p>')
+  var p = doc.find("p")[0]
+
+  var spans = Wapper.wrap({
+    commonAncestorContainer: p,
+    startContainer: p.childNodes[1].firstChild,
+    startOffset: 2,
+    endContainer: p.childNodes[3].firstChild,
+    endOffset: 3
+  })
+
+  equals(doc.html(), '<p>lorem <a href="/">ip<span>sum</span></a><span> </span><a href="/"><span>dol</span>or</a> sit amet.</p>')
+  assertSameNodes(spans, doc.find("span").get())
+})
+
 test("spanning nodes", function() {
   var doc = $("<div>").html('<p>lorem ipsum <a href="/">dolor</a> </p><p>sit amet.</p>')
   var p1 = doc.find("p:first")[0]
