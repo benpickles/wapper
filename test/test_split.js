6bc6a7b316be81ec062f261ce43659a55b7b2189
Ben Pickles <spideryoung@gmail.com>
2011-03-17 17:04:39 +0000

    A couple more tests for common selections.

--- test/test_split.js
+++ test/test_split.js
@@ -73,6 +73,36 @@ test("starting in an inline element", function() {
   ])
 })
 
+test("starting in an inline element, encompassing nested", function() {
+  var doc = $("<div>").html('<p>lorem <a href="/">ipsum</a> <a href="/">dolor</a> sit amet.</p>')
+  var p = doc.find("p")[0]
+
+  var elems = Wapper.split({
+    commonAncestorContainer: p,
+    startContainer: p.childNodes[1].firstChild,
+    startOffset: 2,
+    endContainer: p.childNodes[4],
+    endOffset: 4
+  })
+
+  assertSameNodes(elems, [
+    p.childNodes[1].lastChild,
+    p.childNodes[2],
+    p.childNodes[3],
+    p.childNodes[4],
+  ])
+  assertChildNodeTypes(p, [3, 1, 3, 1, 3, 3])
+  assertChildNodeTypes(p.childNodes[1], [3, 3])
+  assertChildNodeText(p, [
+    "lorem ",
+    "ipsum",
+    " ",
+    "dolor",
+    " sit",
+    " amet."
+  ])
+})
+
 test("ending in an inline element", function() {
   var doc = $("<div>").html('<p>lorem ipsum <a href="/">dolor</a> sit amet.</p>')
   var p = doc.find("p")[0]
@@ -96,6 +126,36 @@ test("ending in an inline element", function() {
     "ipsum ",
     "dolor",
     " sit amet."
+  ])
+})
+
+test("ending in an inline element, encompassing nested", function() {
+  var doc = $("<div>").html('<p>lorem ipsum <a href="/">dolor</a> <a href="/">sit</a> amet.</p>')
+  var p = doc.find("p")[0]
+
+  var elems = Wapper.split({
+    commonAncestorContainer: p,
+    startContainer: p.firstChild,
+    startOffset: 6,
+    endContainer: p.childNodes[3].firstChild,
+    endOffset: 2
+  })
+
+  assertSameNodes(elems, [
+    p.childNodes[1],
+    p.childNodes[2],
+    p.childNodes[3],
+    p.childNodes[4].firstChild,
+  ])
+  assertChildNodeTypes(p, [3, 3, 1, 3, 1, 3])
+  assertChildNodeTypes(p.childNodes[4], [3, 3])
+  assertChildNodeText(p, [
+    "lorem ",
+    "ipsum ",
+    "dolor",
+    " ",
+    "sit",
+    " amet."
   ])
 })
 
